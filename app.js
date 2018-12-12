02423c73f6c2dfb270fab019c000a481c8721fe4
Nibesh Khadka <nibeshkhadka@gmail.com>
2018-12-12 19:20:47 +0200

    Node JS basic completion

---
+++ app.js
@@ -0,0 +1,10 @@
+const http = require("http"),
+ routes = require('./routes'),
+ express = require('express');
+
+ const app = express()
+
+
+//const server = http.createServer(routes);
+
+app.listen(3000);
